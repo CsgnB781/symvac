2c2d1b645a87c125d2de46bb80b8a3d29e353be6
Beïza COSGUN <bcosgun781@gmail.com>
2026-06-25 14:56:43 +0200

    Formulaire pour créer un vacataire

--- src/Controller/VacataireController.php
+++ src/Controller/VacataireController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Vacataire;
+use App\Form\VacataireType;
 use App\Repository\VacataireRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -24,6 +27,27 @@ final class VacataireController extends AbstractController
 
         return $this->render('pages/vacataire/index.html.twig', [
             'vacataires' => $vacataires,
+        ]);
+    }
+
+    #[Route('/vacataire/nouveau', 'vacataire_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $manager): Response
+    {
+        $vacataire = new Vacataire();
+        $form = $this->createForm(VacataireType::class, $vacataire);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $vacataire = $form->getData();
+            $manager->persist($vacataire);
+            $manager->flush();
+
+            $this->addFlash('success', 'Vos changements ont été enregistrés !');
+            return $this->redirectToRoute('app_vacataire');
+        }
+
+        return $this->render('pages/vacataire/new.html.twig', [
+            'form' => $form,
         ]);
     }
 }
